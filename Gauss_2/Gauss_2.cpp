e5117e8ef120beacb26bc54d55614b12df1fb504
edirab <edirab@yandex.ru>
2020-04-01 16:39:48 +0300

    first task finished

--- Gauss_2/Gauss_2.cpp
+++ Gauss_2/Gauss_2.cpp
@@ -6,6 +6,27 @@
 
 using namespace std;
 
+void printVector(float vector_[4], int n) {
+
+	for (int L = 0; L < n; L++) {
+		cout << vector_[L] << " ";
+	}
+	cout << "\n\n";
+}
+
+void printMatrix(float matrixA[4][4], int n) {
+	cout << "Matrix A: \n";
+
+	for (int l = 0; l < n; l++) {
+		for (int m = 0; m < n; m++)
+			cout << matrixA[l][m] << " ";
+
+		cout << "\n";
+	}
+	cout << "\n\n";
+	return;
+}
+
 int main() {
 
 	setlocale(0, "");
@@ -21,29 +42,47 @@ int main() {
 	float vectorX[4] = { 0 };
 
 	int n = sizeof(matrixA) / sizeof(matrixA[0]);
-	//cout << n;
 
 	// прямой ход
-	
 	//указывает на диагональный элемент
 	for (int k = 0; k < n-1; k++) {
+
+		//Выбираем наибольший в столбце
+		int maxInCol_index = k;
+		for (int m = k; m < n; m++) {
+			if (matrixA[m][k] > matrixA[k][k]) {
+				maxInCol_index = m;
+			}
+		}
+
+		if (maxInCol_index != k) {
+
+			float temp = vectorB[k];
+			vectorB[k] = vectorB[maxInCol_index];
+			vectorB[maxInCol_index] = temp;
+
+			for (int r = 0; r < n; r++) {
+				temp = matrixA[k][r];
+				matrixA[k][r] = matrixA[maxInCol_index][r];
+				matrixA[maxInCol_index][r] = temp;
+			}
+		}
+		cout << "Проход " << k+1 << ":\n\n";
+		printMatrix(matrixA, n);
 
 		//указывает на строку
 		for (int i = k+1; i < n; i++) {
 
 			float u = matrixA[i][k] / matrixA[k][k];
-			//j++;
 
-			cout << "Строка " << i << ": " << u << "\n\n";
+			cout << "Строка " << i << ": u (" << i << ", " << k << ") = " << u << "\n";
 
 			// Проходим строку целиком по всем столбцам
 			for (int j = 0; j < n; j++) {
 				matrixA[i][j] = matrixA[i][j] - u * matrixA[k][j];
-				cout << matrixA[i][j] << " ";
 			}
 
 			vectorB[i] = vectorB[i] - u * vectorB[k];
-			cout << "\n";
 		}
 		cout << "\n\n";
 
@@ -56,26 +95,13 @@ int main() {
 			}
 			vectorX[i] = (vectorB[i] - partialSum) / matrixA[i][i];
 		}
-
-		// Print Matrix
-		for (int l = 0; l < n; l++) {
-			for (int m = 0; m < n; m++)
-				cout << matrixA[l][m] << " ";
-
-			cout << "\n";
-		}
-		cout << "\n\n";
-
-		for (int L = 0; L < n; L++) {
-			cout << vectorB[L] << " ";
-		}
-		cout << "\n\n";
-
-		for (int L = 0; L < n; L++) {
-			cout << vectorX[L] << " ";
-		}
-		cout << "\n";
 	}
+
+	printMatrix(matrixA, n);
+	cout << "Вектор b: \n";
+	printVector(vectorB, n);
+	cout << "Вектор Х:\n";
+	printVector(vectorX, n);
 
 	return 0;
 }
