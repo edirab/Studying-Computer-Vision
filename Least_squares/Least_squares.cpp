e356b949c46f855e4d5ae237426809efe6f69f1d
edirab <edirab@yandex.ru>
2020-04-03 21:18:25 +0300

    least squares finished

---
+++ Least_squares/Least_squares.cpp
@@ -0,0 +1,59 @@
+﻿// Least_squares.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <Eigen/LU>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+const uint8_t n = 10;
+
+//int X[12] = { 27, 31, 43, 58, 69, 86, 102, 111, 122, 137, 18, 176 };
+//int Y[12] = { 71, 64, 52, 41, 33, 23, 17, 12, 2, 0, 87, -5 };
+
+int X[n] = { 27, 31, 43, 58, 69, 86, 102, 111, 122/*, 137*/, 18/*, 176*/};
+int Y[n] = { 71, 64, 52, 41, 33, 23, 17, 12, 2/*, 0*/, 87/*, -5*/};
+
+int main()
+{
+	Eigen::MatrixXf A = Eigen::MatrixXf::Ones(n, 2);
+	Eigen::VectorXf b = Eigen::VectorXf::Ones(n);
+	Eigen::Vector2f x;
+
+	// Используемая модель: u = C* e ^ (kx).
+	// После линеаризации: ln u = ln C + kx
+	for (int i = 0; i < n; i++) {
+		A.row(i) << 1, X[i];
+
+		b[i] = log(Y[i]);
+	}
+
+	cout << "Matrix A: \n" << A << "\n\n";
+	cout << "Vector b: \n" << b << "\n\n";
+
+	x = (A.transpose() * A).lu().solve(A.transpose() * b);
+	double C = exp(x[0]);
+	double k = x[1];
+
+	cout << "The solution using normal equations is:\n";
+	cout << "x1 = C = " << C << "\n";
+	cout <<"x2 = k = "<< k << "\n\n";
+
+	//cout << "The solution using the QR decomposition is:\n" << A.colPivHouseholderQr().solve(b) << endl;
+
+	cout << "Difference with given data: \n\n";
+
+	cout << setw(10) << "u - Y[i]" << setw(10) << "u" << setw(6) << "Y[i]" << "\n\n";
+
+	for (int i = 0; i < n; i++) {
+		double u = C * exp(k * X[i]);
+		
+		cout << setw(10) << u-Y[i] << setw(10) << u << setw(6) << Y[i] << "\n";
+	}
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
---
+++ secondary_task_1/secondary_task_1.cpp
@@ -0,0 +1,43 @@
+﻿// secondary_task_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <Eigen/LU>
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+
+
+using namespace std;
+int main(int argc, char* argv[])
+{
+	Eigen::Matrix4d A, lower, upper;
+	Eigen::Vector4d b, x;
+	A << 10, 6, 2, 0, 5, 1, -2, 4, 3, 5, 1, -1, 0, 6, -2, 2;
+	b << 25, 14, 10, 8;
+	Eigen::PartialPivLU<Eigen::Matrix4d> lu(A);
+
+	cout << "Matrix A" << endl << A << endl;
+	cout << "Vector b" << endl << b << endl;
+	cout << "LU-decomp" << endl << lu.matrixLU() << endl;
+
+	lower = lu.matrixLU().triangularView<Eigen::StrictlyLower>();
+	lower = lower + Eigen::Matrix4d::Identity();
+	upper = lu.matrixLU().triangularView<Eigen::Upper>();
+
+	cout << "Matrix lower" << endl << lower << endl;
+	cout << "Matrix upper" << endl << upper << endl;
+	cout << "Multiplication" << endl << lower * upper << endl;
+	cout << "Multiplication with permutation" << endl
+		<< lu.permutationP().inverse() * lower * upper << endl;
+
+	return 0;
+}
